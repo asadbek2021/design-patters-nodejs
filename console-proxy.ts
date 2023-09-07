bd31a0037e25d4c1838e09c61425b8a8b0a87d86
Asadbek Raimov <asadbek.r@payme.uz>
2023-09-07 12:54:55 +0500

    feat(proxy): console timestamp proxy

---
+++ console-proxy.ts
@@ -0,0 +1,10 @@
+const consoleProxy = new Proxy(console, {
+    get: (target, property) => {
+        return (...args) => {
+            const timestamp = new Date();
+            target[property](`${timestamp.toISOString()} | `,...args)
+        }
+    }
+})
+
+consoleProxy.group('Hallo! I`m Steven',343434)
